ccdf1e0b4a91d5e2af5d6f2f7cb3829daea3aacc
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-05-15 09:00:51 +0900

    Add files

---
+++ mouse/src/run.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+
+#include "run.h"
+
+LRESULT keyboardCallback(int code, WPARAM wParam, LPARAM lParam) {
+  printf("Called hook handler\n");
+  return CallNextHookEx(0, code, wParam, lParam);
+}
+
+int run() {
+  HHOOK hHook = SetWindowsHookEx(WH_MOUSE_LL, keyboardCallback, 0, 0);
+
+  if (hHook == nullptr) {
+    printf("Failed to call SetWindowsHookEx\n");
+    return -1;
+  }
+
+  MSG msg{};
+
+  while (GetMessage(&msg, 0, 0, 0) != 0) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  if (UnhookWindowsHookEx(hHook) == 0) {
+    printf("Failed to call UnhookWindowsHookEx()\n");
+  }
+
+  printf("Done\n");
+
+  return 0;
+}
